a2788c9551c456665e2bb43b7762882f5874adcc
Alex Gusev <alex@flancer64.com>
2021-04-15 11:49:40 +0300

    WF-445 Add email plugin

--- init/dev/manual/DevEnv.mjs
+++ init/dev/manual/DevEnv.mjs
@@ -2,25 +2,16 @@
  * Initialize development environment to run dev tests.
  */
 import $path from 'path';
-import $url from 'url';
 import Container from '@teqfw/di';
 
-/* Resolve paths to main folders */
-const {path: currentScript} = $url.parse(import.meta.url);
-const pathScript = $path.dirname(currentScript);
+/* Create and setup DI container (DI own namespace) */
+const url = new URL(import.meta.url);
+const pathScript = $path.dirname(url.pathname);
 const pathPrj = $path.join(pathScript, '../..');
-const pathNode = $path.join(pathPrj, 'node_modules');
-const srcTeqFwCore = $path.join(pathNode, '@teqfw/core-app/src');
-const srcTeqFwDi = $path.join(pathNode, '@teqfw/di/src');
-const srcFl32User = $path.join(pathNode, '@flancer32/teq_user/src');
-/* Create and setup DI container (once per all imports) */
-
+const srcTeqFwDi = $path.join(pathPrj, 'node_modules/@teqfw/di/src');
 /** @type {TeqFw_Di_Container} */
 const container = new Container();
-// add backend sources to map
-container.addSourceMapping('TeqFw_Core_App', srcTeqFwCore, true, 'mjs');
 container.addSourceMapping('TeqFw_Di', srcTeqFwDi, true, 'mjs');
-container.addSourceMapping('Fl32_Teq_User', srcFl32User, true, 'mjs');
 
 /**
  * Setup development environment (if not set before) and return DI container.
@@ -29,16 +20,28 @@ container.addSourceMapping('Fl32_Teq_User', srcFl32User, true, 'mjs');
  */
 export default async function init() {
     // DEFINE INNER FUNCTIONS
-
-    async function initConfig(container, rootPath) {
+    /**
+     * Load local configuration (/cfg/local.json).
+     *
+     * @param {TeqFw_Di_Container} container
+     * @param {String} path
+     * @return {Promise<void>}
+     */
+    async function initConfig(container, path) {
         /** @type {TeqFw_Core_App_Config} */
         const config = await container.get('TeqFw_Core_App_Config$');
-        config.load({rootPath});  // load local configuration
+        config.load({rootPath: path});  // load local configuration
         /** @type {TeqFw_Core_App_Defaults} */
         const DEF = await container.get('TeqFw_Core_App_Defaults$');
         container.set(DEF.DI_CONFIG, config.get());
     }
 
+    /**
+     * Connect to database.
+     *
+     * @param {TeqFw_Di_Container} container
+     * @return {Promise<void>}
+     */
     async function initDb(container) {
         /** @type {TeqFw_Core_App_Db_Connector} */
         const rdb = await container.get('TeqFw_Core_App_Db_Connector$');  // singleton instance
@@ -50,6 +53,30 @@ export default async function init() {
         // registry.register(rdb, 'payload');
     }
 
+    /**
+     * Scan project for TeqFW plugins and add namespaces to DI container.
+     *
+     * @param {TeqFw_Di_Container} container
+     * @param {String} path
+     * @return {Promise<void>}
+     */
+    async function initDi(container, path) {
+        /** @type {TeqFw_Di_Util_PluginScanner} */
+        const scanner = await container.get('TeqFw_Di_Util_PluginScanner$');
+        const spaces = await scanner.getNamespaces(path);
+        for (const [ns, entry] of Object.entries(spaces)) {
+            /** @type {TeqFw_Di_Api_ResolveDetails} (to use IDE autocomplete) */
+            const data = entry;
+            container.addSourceMapping(data.ns, data.path, data.isAbsolute, data.ext);
+        }
+    }
+
+    /**
+     * Setup logger to use console transport.
+     *
+     * @param {TeqFw_Di_Container} container
+     * @return {Promise<void>}
+     */
     async function initLogger(container) {
         /** @type {TeqFw_Core_App_Logger} */
         const logger = await container.get('TeqFw_Core_App_Logger$');
@@ -59,13 +86,15 @@ export default async function init() {
     }
 
     // MAIN FUNCTIONALITY
-    /** @type {TeqFw_Core_App_Config} */
-    const config = await container.get('TeqFw_Core_App_Config$');
-    if (Object.keys(config.get()).length === 0) {
-        // init env if has not been initiated before
+
+    // init env if has not been initiated before (one namespace only can be resolved)
+    const {logicalNs} = container.getNsResolver().list();
+    if (Object.keys(logicalNs).length <= 1) {
+        await initDi(container, pathPrj);
         await initConfig(container, pathPrj);
         await initLogger(container);
         await initDb(container);
     }
+
     return container;
 }
